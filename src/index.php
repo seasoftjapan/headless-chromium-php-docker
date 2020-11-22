b0765c3a9eee7b86e1a11ef06fde5dff08b5e1e4
flaurianti <flaurianti@tierratelematics.com>
2020-11-22 19:58:52 +0100

    headless = true is default, set user agent

--- src/index.php
+++ src/index.php
@@ -6,11 +6,12 @@ use HeadlessChromium\BrowserFactory;
 $browserFactory = new BrowserFactory("/usr/bin/google-chrome");
 
 $browser = $browserFactory->createBrowser([
-	'headless'					=> true,
 	'sendSyncDefaultTimeout'	=> 30000
 ]);
 
 $page = $browser->createPage();
+
+$page->setUserAgent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36');
 
 $page->navigate("https://en.wikipedia.org/wiki/HelloWorld")->waitForNavigation();
 
